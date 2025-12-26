c024765e2806d7e152222766752c059a4184ee24
William Skiles <williamskiles@Williams-MacBook-Air.local>
2025-12-26 07:08:00 -0600

    Add URL callback handler for Strava OAuth deep link
    - Added onOpenURL handler to catch velomind://strava callback
    - ASWebAuthenticationSession will automatically intercept the callback
    - Enables OAuth flow to complete when Strava redirects back to app

--- VeloMind/VeloMindApp.swift
+++ VeloMind/VeloMindApp.swift
@@ -18,6 +18,13 @@ struct VeloMindApp: App {
                             rideCoordinator.persistenceManager.setCurrentUser(userId)
                         }
                     }
+                    .onOpenURL { url in
+                        // Handle Strava OAuth callback
+                        if url.scheme == "velomind" && url.host == "strava" {
+                            // URL will be handled by ASWebAuthenticationSession automatically
+                            print("Received Strava callback: \(url)")
+                        }
+                    }
             } else {
                 LoginView()
                     .environmentObject(authManager)
